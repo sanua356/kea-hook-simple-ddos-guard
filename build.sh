a2d477b82ac3dd19cf423598b25ba66ee86f9e0f
sanua356 <sanek.pankratov356@gmail.com>
2025-10-24 08:55:58 +0300

    Add gitignore, remove ggdb from build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-g++ -ggdb -I. -I /usr/include/kea -L /usr/lib/kea/lib -fpic -shared -o simple_ddos_guard.so \
+g++ -I. -I /usr/include/kea -L /usr/lib/kea/lib -fpic -shared -o simple_ddos_guard.so \
   load_unload.cc pkt4_receive.cc version.cc simple_ddos_guard_messages.cc simple_ddos_guard_log.cc  \
   -lkea-dhcpsrv -lkea-dhcp -lkea-hooks -lkea-log -lkea-util -lkea-exceptions -lcurl
